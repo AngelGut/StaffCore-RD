5e32d51a27fa066b855cbfa99e446da23d315d7f
AngelGutierrez <angeldavid.guti.con>
2026-08-03 16:38:33 -0400

    Arreglando tema con los nuggets, se acomodo ligeramente el codigo en staff y se creo su db

---
+++ StaffCore RD/Data/StaffDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StaffCoreRD.Models;
+using System.Reflection.Emit;
+
+namespace StaffCoreRD.Data
+{
+    public class StaffDbContext : IdentityDbContext
+    {
+        public StaffDbContext(DbContextOptions<StaffDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Staff> Personal { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed data - 2 empleados de departamentos distintos
+            modelBuilder.Entity<Staff>().HasData(
+                new Staff
+                {
+                    Id = 1,
+                    Nombre = "Juan Carlos Pérez",
+                    Cedula = "001-1234567-8",
+                    Cargo = "Jefe de Tecnología",
+                    Departamento = "Tecnología",
+                    Salario = 50000,
+                    FechaIngreso = new DateTime(2023, 1, 15),
+                    Activo = true
+                },
+                new Staff
+                {
+                    Id = 2,
+                    Nombre = "María Martínez Rodríguez",
+                    Cedula = "002-9876543-2",
+                    Cargo = "Especialista de RRHH",
+                    Departamento = "Recursos Humanos",
+                    Salario = 35000,
+                    FechaIngreso = new DateTime(2023, 3, 20),
+                    Activo = true
+                }
+            );
+        }
+    }
+}
--- StaffCore RD/Models/Staff.cs
+++ StaffCore RD/Models/Staff.cs
@@ -1,15 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-public class Staff
+namespace StaffCoreRD.Models
 {
-    public int Id { get; set; }
-    [Required] public string Nombre { get; set; }  // Nombre completo
-    [Required] public string Cedula { get; set; }  // Formato: 001-0000000-0
-    [Required] public string Cargo { get; set; }  // Ej: Analista de Sistemas
-    [Required] public string Departamento { get; set; }  // Tecnología / RRHH / Finanzas / Operaciones
-    [Required]
-    [Range(23223, double.MaxValue, ErrorMessage = "Mínimo RD$23,223")]
-    public decimal Salario { get; set; }
-    public DateTime FechaIngreso { get; set; }
-    public bool Activo { get; set; } = true;
+    public class Staff
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; }  // Nombre completo
+
+        [Required(ErrorMessage = "La cédula es requerida")]
+        [RegularExpression(@"^\d{3}-\d{7}-\d{1}$",
+            ErrorMessage = "Formato de cédula inválido (001-0000000-0)")]
+        public string Cedula { get; set; }  // Formato: 001-0000000-0
+
+        [Required(ErrorMessage = "El cargo es requerido")]
+        public string Cargo { get; set; }  // Ej: Analista de Sistemas
+
+        [Required(ErrorMessage = "El departamento es requerido")]
+        public string Departamento { get; set; }  // Tecnología / RRHH / Finanzas / Operaciones
+
+        [Required(ErrorMessage = "El salario es requerido")]
+        [Range(23223, double.MaxValue, ErrorMessage = "Mínimo RD$23,223")]
+        public decimal Salario { get; set; }
+
+        public DateTime FechaIngreso { get; set; }
+
+        public bool Activo { get; set; } = true;
+    }
 }
