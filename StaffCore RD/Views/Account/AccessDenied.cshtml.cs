2a4c7da3aeab1368ad0121fc891a09ca153e0148
AngelGutierrez <angeldavid.guti.con>
2026-08-04 15:51:07 -0400

    archivos del view

---
+++ StaffCore RD/Views/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StaffCore_RD.Views.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
